751391c33a0266ebccdf431a905e0d1d88960226
Kevin Bell <bellKevin@pm.me>
2022-05-11 03:20:35 +0000

    cleaning up the format and comments

--- sumOfLikeNumbers/src/sumoflikenumbers/SumOfLikeNumbers.java
+++ sumOfLikeNumbers/src/sumoflikenumbers/SumOfLikeNumbers.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class SumOfLikeNumbers {
 
     public static void main(String[] args) {
-        // TODO code application logic here
         System.out.println("Sum of Like Numbers by Kevin Bell \n");
         Scanner computerKeyboardInput = new Scanner(System.in);
         int i;
