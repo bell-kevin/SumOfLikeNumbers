ca497ac94e87baf7d805efed7800062aaf348116
Kevin Bell <bellKevin@pm.me>
2022-05-11 15:03:14 +0000

    made code more readable

--- sumOfLikeNumbers/src/sumoflikenumbers/SumOfLikeNumbers.java
+++ sumOfLikeNumbers/src/sumoflikenumbers/SumOfLikeNumbers.java
@@ -7,21 +7,19 @@ public class SumOfLikeNumbers {
     public static void main(String[] args) {
         System.out.println("Sum of Like Numbers by Kevin Bell \n");
         Scanner computerKeyboardInput = new Scanner(System.in);
-        int i;
-        int sum = 0;
-        int max, num;
+        int index, sum = 0, maximum, number;
         String value;
         System.out.print("Please enter a number: ");
-        max = computerKeyboardInput.nextInt();
-        if (max % 2 == 0) {
+        maximum = computerKeyboardInput.nextInt();
+        if (maximum % 2 == 0) {
             num = 2;
             value = "EVEN";
         } else {
-            num = 1;
+            number = 1;
             value = "ODD";
         }
-        for (i = num; i <= max; i += 2) {
-            sum += i;
+        for (index = number; index <= maximum; index += 2) {
+            sum += index;
         }
         System.out.println("The sum of " + value + " numbers is " + sum);
     }
