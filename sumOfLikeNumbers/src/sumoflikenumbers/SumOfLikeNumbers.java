a665e9978574943b1429eb9089396b72f64fcb06
Kevin Bell <bellKevin@pm.me>
2022-05-11 03:13:31 +0000

    correct code
    got help from teacher - this is the correct code

--- sumOfLikeNumbers/src/sumoflikenumbers/SumOfLikeNumbers.java
+++ sumOfLikeNumbers/src/sumoflikenumbers/SumOfLikeNumbers.java
@@ -1,39 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sumoflikenumbers;
 
 import java.util.Scanner;
 
-/**
- *
- * @author 4800590195
- */
 public class SumOfLikeNumbers {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Sum of Like Numbers by Kevin Bell \n");
         Scanner computerKeyboardInput = new Scanner(System.in);
-        int i, number = 0, sum = 0;
-        for (i = 0; number < 1; i++) {
-            System.out.print("Please enter a number: ");
-            number = computerKeyboardInput.nextInt();
-            if (number < 0) {
-                System.out.print("Good bye! \n");
-                break;
-            } else if (number % 2 == 0) {
-                number = number + number;
-                System.out.println("The sum of EVEN numbers is " + number);
-            } else if (!(number % 2 == 0)) {
-                number = sum + number;
-                System.out.println("The sum of ODD numbers is " + sum);
-            }
+        int i;
+        int sum = 0;
+        int max, num;
+        String value;
+
+        System.out.print("Please enter a number: ");
+        max = computerKeyboardInput.nextInt();
+        if (max % 2 == 0) {
+            num = 2;
+            value = "EVEN";
+        } else {
+            num = 1;
+            value = "ODD";
         }
+        for (i = num; i <= max; i += 2) {
+            sum += i;
+        }
+
+        System.out.println("The sum of " + value + " numbers from 0 up to " + max + " is equal to: " + sum);
     }
 }
